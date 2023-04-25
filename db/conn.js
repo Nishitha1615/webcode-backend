b1b467ba7ec7584579e66f4a164c5cc42c9c6ae0
Nishitha1615 <nishitham0@gmail.com>
2023-04-25 17:55:08 +0530

    web code backend

---
+++ db/conn.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+
+const DB="mongodb+srv://nishitha:yhu65pRQk7KnVaE3@cluster0.602gwxp.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(DB,{
+    useUnifiedTopology: true,
+    
+    useNewUrlParser: true,}).then(()=>console.log("Connected")).catch((error)=>console.log(error));
+
